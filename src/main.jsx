6e70085749a30e4e124f769f19753ea94d8c5a03
devankitmishra <ankitmishrapuri@gmail.com>
2024-11-01 20:34:07 +0530

    "Added react-router-dom dependency and implemented routing in main.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom'
+import Home from './routes/home/Home';
+import Dashboard from './routes/dashboard/Dashboard';
+import Chat from './routes/chat/Chat';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/dashboard",
+    children:[
+      {path:"/dashboard",element: <Dashboard/>},
+      {path: "/dashboard/chats/:id", element: <Chat/>},]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
